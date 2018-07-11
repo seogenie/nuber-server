c4500fc830157f29dd7051e5d3c546538b5d35f0
SEOJIN <seogenie@naver.com>
2018-07-11 17:43:08 +0900

    graphql yoga loacalhost 4000 play ground

---
+++ src/api/Hello/sayBye/sayBye.resolvers.ts
@@ -0,0 +1,7 @@
+const resolvers = {
+    Query : {
+        sayBye: () => "hello how r u22"
+    }
+}
+
+export default resolvers;
---
+++ src/api/Hello/sayHello/sayHello.resolvers.ts
@@ -0,0 +1,7 @@
+const resolvers = {
+    Query : {
+        sayHello: () => "hello how r u"
+    }
+}
+
+export default resolvers;

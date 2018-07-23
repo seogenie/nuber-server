cc4bcdf42c63797799fca9397f9dad751818930d
seogenie <seogenie@naver.com>
2018-07-23 16:14:27 +0900

    41 create custom jwt with strong password generator

---
+++ src/utils/createJWT.ts
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken"
+
+const createJWT = (userId: number) : string => {
+    const token = jwt.sign(
+        {
+            id: userId
+        },
+        process.env.JWT_TOKEN
+    )
+    return token
+}
+
+export default createJWT

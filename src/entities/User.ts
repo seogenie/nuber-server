21baa97d19375676b7803d04f82e5a147efc06a4
seogenie <seogenie@naver.com>
2018-07-25 15:00:22 +0900

    get my places

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -71,11 +71,11 @@ class User extends BaseEntity {
   @Column({ type: "text", nullable: true })
   fbId: string;
 
-//   @OneToMany(type => Chat, chat => chat.passenger)
-//   chatsAsPassenger: Chat[];
+  // @OneToMany(type => Chat, chat => chat.passenger)
+  // chatsAsPassenger: Chat[];
 
-//   @OneToMany(type => Chat, chat => chat.driver)
-//   chatsAsDriver: Chat[];
+  // @OneToMany(type => Chat, chat => chat.driver)
+  // chatsAsDriver: Chat[];
 
   @OneToMany(type => Message, message => message.user)
   messages: Message[];
